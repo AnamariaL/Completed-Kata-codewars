6e149865900dbe3823ffcc849e3f13adc2f01b2b
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2019-08-18 21:14:46 +0300

    Update Hello_World_Without_Strings.py

--- Hello_World_Without_Strings.py
+++ Hello_World_Without_Strings.py
@@ -1,2 +1,12 @@
+"""Task
+You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+You cannot use the following:
+
+"Hello, World!"
+'Hello, World!'
+`Hello, World!`
+Good luck and try to be as creative as possible!
+"""
 def hello_world():
   return chr(72) + chr(101) + chr(108) + chr(108) + chr(111) + chr(44) + chr(32) + chr(87) + chr(111) + chr(114) + chr(108) + chr(100) + chr(33)
