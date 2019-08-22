82d7c6dd9c7803d921634c18ce687e6db6282b91
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2019-08-23 00:08:17 +0300

    Update Remove duplicate words.py

--- Remove duplicate words.py
+++ Remove duplicate words.py
@@ -1,3 +1,15 @@
+"""Description:
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'"""
 def remove_duplicate_words(s):
     words = s.split()
     return " ".join(sorted(set(words), key=words.index))
