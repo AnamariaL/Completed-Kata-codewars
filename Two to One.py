a14be606afc58c7b1b72f93ca4355d738e731ed6
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2019-08-28 17:32:24 +0300

    Update Two to One.py

--- Two to One.py
+++ Two to One.py
@@ -9,5 +9,4 @@ longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz""""
 def longest(s1, s2):
-    s3 = s1+s2
-    return ''.join(sorted(set(s3)))
+    return ''.join(sorted(set(s1+s2)))
