8acb181b2b9465184435caa9c4f8bd43c7d1a940
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2019-08-28 16:38:26 +0300

    Update powers_of_3.py

--- powers_of_3.py
+++ powers_of_3.py
@@ -1,3 +1,11 @@
+"""Description:
+Given a positive integer N, return the largest integer k such that 3^k < N.
+
+For example,
+
+largestPower(3) = 0
+largestPower(4) = 1
+You may assume that the input to your function is always a positive integer."""
 def largestPower(N):
     k=0
     while pow(3,k)<N:
